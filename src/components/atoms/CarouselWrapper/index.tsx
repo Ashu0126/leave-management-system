7b8d4ad429af615f5d3c4fae37cd55f1fb86a8b1
Ashu0126 <ashish.jaryal14@gmail.com>
2024-02-13 09:25:39 +0530

    style changes

--- src/components/atoms/CarouselWrapper/index.tsx
+++ src/components/atoms/CarouselWrapper/index.tsx
@@ -3,32 +3,34 @@ import style from "./index.module.scss";
 
 const CarouselWrapper = () => {
   return (
-    <Carousel controls={false} indicators={false}>
-      <CarouselItem>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
-        voluptate, consequatur sapiente voluptatem quam totam quos. Ex itaque
-        totam porro, blanditiis modi sed? Ab eligendi suscipit, dolorem
-        voluptatibus amet maiores.
-      </CarouselItem>
-      <CarouselItem>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
-        voluptate, consequatur sapiente voluptatem quam totam quos. Ex itaque
-        totam porro, blanditiis modi sed? Ab eligendi suscipit, dolorem
-        voluptatibus amet maiores.
-      </CarouselItem>
-      <CarouselItem>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
-        voluptate, consequatur sapiente voluptatem quam totam quos. Ex itaque
-        totam porro, blanditiis modi sed? Ab eligendi suscipit, dolorem
-        voluptatibus amet maiores.
-      </CarouselItem>
-      <CarouselItem>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
-        voluptate, consequatur sapiente voluptatem quam totam quos. Ex itaque
-        totam porro, blanditiis modi sed? Ab eligendi suscipit, dolorem
-        voluptatibus amet maiores.
-      </CarouselItem>
-    </Carousel>
+    <div className={style.carousel}>
+      <Carousel indicators={false}>
+        <CarouselItem>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+          voluptate, consequatur sapiente voluptatem quam totam quos. Ex itaque
+          totam porro, blanditiis modi sed? Ab eligendi suscipit, dolorem
+          voluptatibus amet maiores.
+        </CarouselItem>
+        <CarouselItem>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+          voluptate, consequatur sapiente voluptatem quam totam quos. Ex itaque
+          totam porro, blanditiis modi sed? Ab eligendi suscipit, dolorem
+          voluptatibus amet maiores.
+        </CarouselItem>
+        <CarouselItem>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+          voluptate, consequatur sapiente voluptatem quam totam quos. Ex itaque
+          totam porro, blanditiis modi sed? Ab eligendi suscipit, dolorem
+          voluptatibus amet maiores.
+        </CarouselItem>
+        <CarouselItem>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero
+          voluptate, consequatur sapiente voluptatem quam totam quos. Ex itaque
+          totam porro, blanditiis modi sed? Ab eligendi suscipit, dolorem
+          voluptatibus amet maiores.
+        </CarouselItem>
+      </Carousel>
+    </div>
   );
 };
 
